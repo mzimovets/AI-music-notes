9f1ceaac951bb17b6e9f4e4f6b432dfa044ad07d
Victor Popov <popov.v@volgablob.ru>
2026-02-18 15:26:06 +0300

    Вынесен Поиск песен на главной странице в отдельный компонент

---
+++ app/home/search/columns.ts
@@ -0,0 +1,5 @@
+export const columns = [
+  { name: "НАЗВАНИЕ", uid: "name", align: "start" },
+  { name: "АВТОР", uid: "role", align: "center" },
+  { name: "ДЕЙСТВИЯ", uid: "actions", align: "end" },
+];
