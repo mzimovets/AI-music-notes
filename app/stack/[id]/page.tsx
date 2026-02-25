ee627adb93193a193d5e29b3cbc274166a23756a
Victor Popov <popov.v@volgablob.ru>
2026-02-25 20:11:01 +0300

    Исправлено отображение кондаков в редактированиии стопки

--- app/stack/[id]/page.tsx
+++ app/stack/[id]/page.tsx
@@ -34,55 +34,55 @@ const mealFilesMap: Record<string, { start: string; end: string }> = {
   },
   rozhdestvo: {
     start: "meals-pdf/rozhdestvo-trop.pdf",
-    end: "meals-pdf/rozhdestvo-cond.pdf",
+    end: "meals-pdf/rozhdestvo-kond.pdf",
   },
   kreshchenie: {
     start: "meals-pdf/kreshchenie-trop.pdf",
-    end: "meals-pdf/kreshchenie-cond.pdf",
+    end: "meals-pdf/kreshchenie-kond.pdf",
   },
   sretenie: {
     start: "meals-pdf/sretenie-trop.pdf",
-    end: "meals-pdf/sretenie-cond.pdf",
+    end: "meals-pdf/sretenie-kond.pdf",
   },
   blagoveshchenie: {
     start: "meals-pdf/blagoveshchenie-trop.pdf",
-    end: "meals-pdf/blagoveshchenie-cond.pdf",
+    end: "meals-pdf/blagoveshchenie-kond.pdf",
   },
   vhod: {
     start: "meals-pdf/vhod-trop.pdf",
-    end: "meals-pdf/vhod-cond.pdf",
+    end: "meals-pdf/vhod-kond.pdf",
   },
   pascha: {
     start: "meals-pdf/pascha-trop.pdf",
-    end: "meals-pdf/pascha-cond.pdf",
+    end: "meals-pdf/pascha-kond.pdf",
   },
   voznesenie: {
     start: "meals-pdf/voznesenie-trop.pdf",
-    end: "meals-pdf/voznesenie-cond.pdf",
+    end: "meals-pdf/voznesenie-kond.pdf",
   },
   troica: {
     start: "meals-pdf/troica-trop.pdf",
-    end: "meals-pdf/troica-cond.pdf",
+    end: "meals-pdf/troica-kond.pdf",
   },
   preobrazhenie: {
     start: "meals-pdf/preobrazhenie-trop.pdf",
-    end: "meals-pdf/preobrazhenie-cond.pdf",
+    end: "meals-pdf/preobrazhenie-kond.pdf",
   },
   uspenie: {
     start: "meals-pdf/uspenie-trop.pdf",
-    end: "meals-pdf/uspenie-cond.pdf",
+    end: "meals-pdf/uspenie-kond.pdf",
   },
   rozhdestvoBogorodicy: {
     start: "meals-pdf/rozhdestvoBogorodicy-trop.pdf",
-    end: "meals-pdf/rozhdestvoBogorodicy-cond.pdf",
+    end: "meals-pdf/rozhdestvoBogorodicy-kond.pdf",
   },
   vozdvizhenie: {
     start: "meals-pdf/vozdvizhenie-trop.pdf",
-    end: "meals-pdf/vozdvizhenie-cond.pdf",
+    end: "meals-pdf/vozdvizhenie-kond.pdf",
   },
   vvedenie: {
     start: "meals-pdf/vvedenie-trop.pdf",
-    end: "meals-pdf/vvedenie-cond.pdf",
+    end: "meals-pdf/vvedenie-kond.pdf",
   },
 };
 
